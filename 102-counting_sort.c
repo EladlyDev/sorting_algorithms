f5b796a7873845b6fb71e1e15f8a9a0cbac5731c
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-19 20:05:44 +0300

    Fix count_sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -28,14 +28,8 @@ void counting_sort(int *array, size_t size)
 
 	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
+	print_array(count, max + 1);
 
-	for (i = 0; i <= max; i++)
-	{
-		printf("%d", count[i]);
-		if (i != max)
-			printf(", ");
-	}
-	printf("\n");
 	sorted = malloc(sizeof(int) * size);
 	if (sorted == NULL)
 	{
@@ -44,7 +38,7 @@ void counting_sort(int *array, size_t size)
 	}
 
 	for (i = (int) size - 1; i >= 0; i--)
-		sorted[count[array[i]] - 1] = array[i];
+		sorted[--count[array[i]]] = array[i];
 	for (i = 0; i < (int) size; i++)
 		array[i] = sorted[i];
 	free(sorted), sorted = NULL;
